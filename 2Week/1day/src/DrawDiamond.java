3a5572d34792c33394a9cf3e27ac0b6839486b25
juliagondocs <julia.gondocs@gmail.com>
2018-04-13 17:12:20 +0200

    workshop section done

--- 2Week/1day/src/DrawDiamond.java
+++ 2Week/1day/src/DrawDiamond.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DrawDiamond {
     public static void main ( String [] args) {
         // Write a program that reads a number from the standard input, then draws a
@@ -13,5 +15,29 @@ public class DrawDiamond {
         //    *
         //
         // The diamond should have as many lines as the number was
+        Scanner scanner = new Scanner(System.in);
+        int userInput1 = scanner.nextInt();
+        if (userInput1 % 2 != 0){
+            userInput1+=1;
+        }
+        for(int i=0;i<=userInput1/2;i++) {
+            for(int j=0;j<userInput1-i;j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<(userInput1/2);i++) {
+            for(int j=0;j<i+(userInput1/2)+1;j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=(userInput1/2)-i-1;k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
  }
 }

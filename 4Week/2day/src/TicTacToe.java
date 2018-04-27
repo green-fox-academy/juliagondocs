3e813b4fe691cb676e4fccf50a790192b43bc260
juliagondocs <julia.gondocs@gmail.com>
2018-04-27 19:29:21 +0200

    TicTacToe added

--- 4Week/2day/src/TicTacToe.java
+++ 4Week/2day/src/TicTacToe.java
@@ -33,8 +33,45 @@ public class TicTacToe {
             System.out.println("No such file or directory");
         }
 
-        returnString = lines.get(0);
+        String[][] board = new String[3][3];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = lines.get(i).split("");
+        }
 
-        return returnString;
+        // There is 4 case to win:
+        // ROW Win
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals("X") && board[i][1].equals("X") && board[i][2].equals("X")) {
+                return "X";
+            }
+            if (board[i][0].equals("O") && board[i][1].equals("O") && board[i][2].equals("O")) {
+                return "O";
+            }
+        }
+
+        // Col win
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i].equals("X") && board[1][i].equals("X") && board[2][i].equals("X")) {
+                return "X";
+            }
+            if (board[0][i].equals("O") && board[1][i].equals("O") && board[2][i].equals("O")) {
+                return "O";
+            }
+        }
+        // DIAGONAL 1
+        if (board[0][0].equals("X") && board[1][1].equals("X") && board[2][2].equals("X")) {
+            return "X";
+        }
+        if (board[0][0].equals("O") && board[1][1].equals("O") && board[2][2].equals("O")) {
+            return "O";
+        }
+        // DIAGONAL 2
+        if (board[0][2].equals("X") && board[1][1].equals("X") && board[2][0].equals("X")) {
+            return "X";
+        }
+        if (board[0][2].equals("O") && board[1][1].equals("O") && board[2][0].equals("O")) {
+            return "O";
+        }
+        return "draw";
     }
 }

06083d4316abace46fd8582d971c6ec232d4164c
juliagondocs <julia.gondocs@gmail.com>
2018-04-27 18:58:26 +0200

    TicTacToe added

---
+++ 4Week/2day/src/TicTacToe.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        // Write a function that takes a filename as string,
+        // open and read it. The file data represents a tic-tac-toe
+        // game result. Return 'X'/'O'/'draw' based on which player
+        // has winning situation.
+
+        System.out.println(ticTacResult("win-o.txt"));
+        // should print O
+
+        System.out.println(ticTacResult("win-x.txt"));
+        // should print X
+
+        System.out.println(ticTacResult("draw.txt"));
+        // should print draw
+    }
+
+    public static String ticTacResult(String fileName) {
+        Path path = Paths.get(fileName);
+        String returnString = "";
+
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            System.out.println("No such file or directory");
+        }
+
+        returnString = lines.get(0);
+
+        return returnString;
+    }
+}

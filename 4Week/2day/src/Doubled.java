2cce840a1db6437209a274b543ceb934c8542b22
juliagondocs <julia.gondocs@gmail.com>
2018-04-27 20:50:57 +0200

    Doubled added

---
+++ 4Week/2day/src/Doubled.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+        import java.nio.file.Files;
+        import java.nio.file.Path;
+        import java.nio.file.Paths;
+        import java.util.ArrayList;
+        import java.util.List;
+
+
+public class Doubled {
+
+    public static void main(String[] args) {
+        // Create a method that decrypts the duplicated-chars.txt
+        Path path = Paths.get("duplicated-char.txt");
+
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(path);
+        } catch (IOException e) {
+            System.out.println("No such file or directory");
+        }
+        System.out.println(decrypt(lines));
+    }
+
+    public static ArrayList<String> decrypt(List<String> wrongText) {
+        ArrayList<String> correctText = new ArrayList<>();
+        String row = "";
+        for (int i = 0; i < wrongText.size(); i++) {
+            String[] line = new String[wrongText.get(i).toString().length()];
+            for (int j = 0; j < wrongText.get(i).toString().length(); j++) {
+                line = wrongText.get(i).split("");
+            }
+            for (int j = 0; j < line.length; j += 2) {
+                row += line[j];
+            }
+            correctText.add(row);
+        }
+        return correctText;
+    }
+}

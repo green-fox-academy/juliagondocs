fec6926133386a1578c33fcb15c2ee1e7921e2a1
juliagondocs <julia.gondocs@gmail.com>
2018-05-01 19:23:27 +0200

    5th week starts

---
+++ 5Week/1day/src/Trees.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Trees {
+    public static void main(String[] args) {
+
+        ArrayList<Object> trees=new ArrayList<Object>();
+        String treeType = "oak";
+        String leafColor = " brown";
+        int treeAge = 12;
+        String treeSex = "-";
+
+
+        trees.add(treeType);
+        trees.add(leafColor);
+        trees.add(treeAge);
+        trees.add(treeSex);
+        System.out.println(trees);
+    }
+}

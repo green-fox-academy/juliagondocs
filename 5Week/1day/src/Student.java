e5f0a277aae0fae67df3e4cb3efa4eb4aa4e7e8c
juliagondocs <julia.gondocs@gmail.com>
2018-05-04 17:56:41 +0200

    School updated, Teacher completed with teach method

--- 5Week/1day/src/Student.java
+++ 5Week/1day/src/Student.java
@@ -12,6 +12,7 @@ public class Student {
     public static void main(String[] args) {
         Student student = new Student();
         Teacher teacher = new Teacher();
+        teacher.teach(student);
         student.question(teacher);
     }
 }
--- 5Week/1day/src/Teacher.java
+++ 5Week/1day/src/Teacher.java
@@ -2,4 +2,8 @@ public class Teacher {
     public void answer() {
         System.out.println(" The teacher is answering.");
     }
+
+    public void teach (Student student) {
+        System.out.println(" I am teaching");
+    }
 }

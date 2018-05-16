f74302f3a2b641ba29e5c13ef222a25893e6f87d
juliagondocs <julia.gondocs@gmail.com>
2018-05-16 20:49:58 +0200

    CountLetter

--- 7Week/1day/CountLetters/src/CountLetters.java
+++ 7Week/1day/CountLetters/src/CountLetters.java
@@ -7,20 +7,20 @@ public class CountLetters {
         char[] temp = string.toCharArray();
         Arrays.sort(temp);
 
-        dictionary.put(temp[0], 1);
-        for (int i = 1; i < temp.length; i++) {
-            if (temp[i - 1] == temp[i]) {
-                dictionary.put(temp[i], dictionary.get(temp[i]) + 1);
-            } else {
+        for (int i = 0; i < temp.length; i++) {
+            if (dictionary.containsKey(temp[i]) == false) {
                 dictionary.put(temp[i], 1);
+            } else {
+                dictionary.put(temp[i], dictionary.get(temp[i]) + 1);
             }
         }
         return dictionary;
     }
 
     //public static void main(String[] args) {
-    //    String alma = "aaabbb";
+    //    String alma = "aaabbbzzzeee";
     //    HashMap<Character, Integer> result = countLetters(alma);
+    //    System.out.println(result.keySet());
     //    System.out.println(result.get('b'));
     //    System.out.println(result.get('a'));
     //}
